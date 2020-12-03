97ad225df9c7e483d0f2537dbaf1f34ab9962340
samsond <nosmaspoise@gmail.com>
2020-12-03 11:24:15 +0300

    add logging

--- src/main/java/com/example/demo/controllers/ItemController.java
+++ src/main/java/com/example/demo/controllers/ItemController.java
@@ -2,6 +2,10 @@ package com.example.demo.controllers;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.profiler.Profiler;
+import org.slf4j.profiler.TimeInstrument;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,14 +22,24 @@ public class ItemController {
 
 	@Autowired
 	private ItemRepository itemRepository;
+
+	private static Logger logger = LoggerFactory.getLogger(ItemController.class);
+	private static Profiler profiler = new Profiler("ItemController");
 	
 	@GetMapping
 	public ResponseEntity<List<Item>> getItems() {
-		return ResponseEntity.ok(itemRepository.findAll());
+		profiler.start("get items");
+		List<Item> items = itemRepository.findAll();
+//		return ResponseEntity.ok(itemRepository.findAll());
+		TimeInstrument timeElapsed = profiler.stop();
+		timeElapsed.print();
+
+		return ResponseEntity.ok(items);
 	}
 	
 	@GetMapping("/{id}")
 	public ResponseEntity<Item> getItemById(@PathVariable Long id) {
+		logger.info("item id {} is received", id);
 		return ResponseEntity.of(itemRepository.findById(id));
 	}
 	
--- src/main/java/com/example/demo/security/JWTAuthorizationFilter.java
+++ src/main/java/com/example/demo/security/JWTAuthorizationFilter.java
@@ -2,6 +2,8 @@ package com.example.demo.security;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -18,6 +20,8 @@ import static com.example.demo.security.SecurityConstants.*;
 
 public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
 
+    private static Logger logger = LoggerFactory.getLogger(JWTAuthorizationFilter.class);
+
     public JWTAuthorizationFilter(AuthenticationManager authenticationManager) {
         super(authenticationManager);
     }
@@ -27,6 +31,7 @@ public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
         String header = request.getHeader(HEADER_STRING);
 
         if (header == null || !header.startsWith(TOKEN_PREFIX)) {
+            logForbiddenIpAddress(request);
             chain.doFilter(request, response);
             return;
         }
@@ -49,8 +54,21 @@ public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
             if (user != null) {
                 return new UsernamePasswordAuthenticationToken(user, null, new ArrayList<>());
             }
+            logForbiddenIpAddress(request);
             return null;
         }
+        logForbiddenIpAddress(request);
         return null;
+    }
+
+    private void logForbiddenIpAddress(HttpServletRequest request) {
+        String ipAddress = request.getHeader("X-Forward-For");
+
+        if(ipAddress== null){
+
+            ipAddress = request.getRemoteAddr();
+        }
+
+        logger.warn("Forbidden access with client ip address {}", ipAddress);
     }
 }
